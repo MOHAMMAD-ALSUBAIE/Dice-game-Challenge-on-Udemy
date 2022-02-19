ceb1369c84e00511039f13d06eaa1f58d7811cb9
MOHAMMAD-ALSUBAIE <68867495+MOHAMMAD-ALSUBAIE@users.noreply.github.com>
2022-02-19 08:18:29 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+
+
+//first player
+var randomNumber1=Math.floor(Math.random()*6)+1;//it will generate random number when reversh the page
+
+var randomDiscNumber1="images/dice" + randomNumber1 + ".png";
+document.querySelector("img.img1").setAttribute("src",randomDiscNumber1);
+
+
+
+
+
+
+//scanned player
+var randomNumber2=Math.floor(Math.random()*6)+1;//it will generate random number when reversh the page
+
+var randomDiscNumber2="images/dice"+ randomNumber2 + ".png";
+document.querySelector("img.img2").setAttribute("src",randomDiscNumber2);//it will change what in src whit varible discNumber2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if(randomNumber1===randomNumber2){
+  document.querySelector("h1").textContent="Draw!";//it take the textContent inside h1 and change the value, it property
+}
+else if(randomNumber1>randomNumber2){
+  document.querySelector("h1").textContent="🚩 Player 1 win";//it take the textContent inside h1 and change the value, it property
+}
+else {
+  document.querySelector("h1").textContent="Player 2 win 🚩 ";//it take the textContent inside h1 and change the value, it property
+}
